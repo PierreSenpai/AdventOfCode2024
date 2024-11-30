3b2fb38680712f8f3328a517e63c0d73adcf476b
BuildTools <unconfigured@null.spigotmc.org>
2024-11-30 20:23:47 +0100

    implement InputReader, remove test stuff

--- src/main/java/day01/Day01Part01.java
+++ src/main/java/day01/Day01Part01.java
@@ -1,13 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-
 public class Day01Part01 {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new FileReader("src/main/resources/day01.txt"));
-        String line;
-        while ((line = br.readLine()) != null) {
-            System.out.println(line);
-        }
-        br.close();
-    }
+    
 }
