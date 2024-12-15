8781d040e28c9422f72ba357aa6591ea7bcaec62
PierreSenpai <pierre07.walther@gmail.com>
2024-12-15 18:49:35 +0100

    after day 10 part 2

---
+++ src/main/java/day10/Day10Part2.java
@@ -0,0 +1,77 @@
+package day10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import util.InputReader;
+
+public class Day10Part2 {
+    private int[][] map;
+    // stores coordinate pairs for each 0 on the map
+    private List<int[]> pathStarts = new ArrayList<>();
+
+    Day10Part2() {
+        List<String> input = InputReader.readInputByLine("src/main/resources/day10.txt");
+
+        int height = input.size();
+        int width = input.getFirst().length();
+        map = new int[width][height];
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                map[x][y] = Character.getNumericValue(input.get(y).charAt(x));
+            }
+        }
+    }
+
+    public void processInput() {
+        findStarts();
+        int sum = 0;
+        for (int[] start : pathStarts) {
+            sum += calculateRating(start[0], start[1]);
+        }
+        System.out.println(sum);
+    }
+
+    public void findStarts() {
+        // iterates through each x/y location and store pair when it's a path start (0)
+        for (int y = 0; y < map[0].length; y++) {
+            for (int x = 0; x < map.length; x++) {
+                if (map[x][y] == 0)
+                    pathStarts.add(new int[] { x, y });
+            }
+        }
+    }
+
+    public int calculateRating(int x, int y) {
+        return calculateRating(x, y, 0);
+    }
+
+    public int calculateRating(int x, int y, int value) {
+        if (value == 9)
+            return 1;
+
+        // checks for each direction if the adjacent value is greater by 1
+        // if that's the case, it performs the same on the next tile until it reaches 9
+        int sum = 0;
+
+        if (x + 1 < map.length && map[x + 1][y] == value + 1)
+            sum += calculateRating(x + 1, y, value + 1);
+
+        if (y + 1 < map[x].length && map[x][y + 1] == value + 1)
+            sum += calculateRating(x, y + 1, value + 1);
+
+        if (x - 1 >= 0 && map[x - 1][y] == value + 1)
+            sum += calculateRating(x - 1, y, value + 1);
+
+        if (y - 1 >= 0 && map[x][y - 1] == value + 1)
+            sum += calculateRating(x, y - 1, value + 1);
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Day10Part2 d10p2 = new Day10Part2();
+        d10p2.processInput();
+    }
+}
